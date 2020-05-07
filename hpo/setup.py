19f859054d5473642f3b9a5b4de55af7b1d1950b
Robert Bennett <robertbennett1998@outlook.com>
2020-05-07 03:45:39 +0100

    Added missing packeage requirement

--- hpo/setup.py
+++ hpo/setup.py
@@ -9,4 +9,4 @@ setup(name='hpo',
       license='MIT',
       packages=['hpo', 'hpo.strategies', 'hpo.strategies.genetic_algorithm', 'hpo.strategies.bayesian_method', 'hpo.strategies.random_search'],
       zip_safe=False,
-      install_requires=["ray>=0.8.2", "scikit-learn>=0.22.2", "tensorflow>=2.0.0", "numpy>=1.17.3"])
+      install_requires=["ray>=0.8.2", "tqdm>=4.42.0" "scikit-learn>=0.22.2", "tensorflow>=2.0.0", "numpy>=1.17.3"])
